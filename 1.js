a98edff1a0748e3995db34dfe52ec261e1334ce6
Mixchelle <michelle.biologia@hotmail.com>
2022-09-06 10:02:33 -0300

    consegui mudar a class select porem nao passou no teste

--- 1.js
+++ 1.js
@@ -1,19 +1,4 @@
-
-let color1 = document.getElementById('color-1')
-color1.style.backgroundColor = 'black'                    
-let color2 = document.getElementById('color-2')
-color2.style.backgroundColor = 'red'
-let color3 = document.getElementById('color-3')
-color3.style.backgroundColor = 'blue'
-let color4 = document.getElementById('color-4')
-color4.style.backgroundColor = 'pink'
-//Função para gerar cores
+                 
 
 
 
-
-
-    // botão para gerar cores aleatoria
-
- 
- 
--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 
-
 //Função para gerar cores
-
 function generateColors(){
     const chars = '0123456789ABCDEF'
     let color = '#'
     for(let i = 0; i < 6; i += 1) {
     color += chars.charAt(Math.floor(Math.random() * chars.length))
     }
-    return color;
+return color;
 }
+
 
 // botão para gerar cores aleatoria
 document.getElementById('button-random-color').addEventListener('click', function btnColor (){
+    let salvarcores = []   
+      
     for (let i = 2; i < 5; i += 1){
         let corsave = generateColors()
         let divColor = document.getElementById(`color-${i}`)
-        divColor.style.backgroundColor = corsave
+        divColor.style.backgroundColor = corsave;
+    
     }
 })
+ 
 
 
-function savecolor () {
-const storage = JSON.stringify(corsave);
-        localStorage.setItem('colorpallete', corsave)
-
-}
-function addSelect(event) {
-    const select = document.querySelector('.selected');
-    event.target.classList.add('selected');
-    select.classList.remove('selected');
-    input.value = '';
-  }
+  let penColour = 'black';
 
 function setPixelColour (pixel) {
     pixel.style.backgroundColor = penColour;
 }
+
 
 document.getElementById("clear-board").addEventListener('click', function btnclear(){
     const pixels = document.querySelectorAll('.pixel');
@@ -44,15 +38,21 @@ document.getElementById("clear-board").addEventListener('click', function btncle
     }
     }
 )
-function addSelect(event) {
-    const select = document.querySelector('.selected');
-    event.target.classList.add('selected');
-    select.classList.remove('selected');
-    input.value = '';
-  }
-  
 
+const oneLi = document.getElementById('color1')
+const firstLi = document.getElementById('color-2');
+const secondLi = document.getElementById('color-3');
+const thirdLi = document.getElementById('color-4');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
 
-
-let penColour = 'blue';
-
+function handleChangeTech(event) {
+  const techElement = document.querySelector('.selected');
+  techElement.classList.remove('selected');
+  event.target.classList.add('selected');
+  input.value = '';
+}
+oneLi.addEventListener('click', handleChangeTech)
+firstLi.addEventListener('click', handleChangeTech);
+secondLi.addEventListener('click', handleChangeTech);
+thirdLi.addEventListener('click', handleChangeTech);

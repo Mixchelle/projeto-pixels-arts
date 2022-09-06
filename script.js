54bb2da0c6f92535a4d2beefd9d29cbd4230b8f8
Mixchelle <michelle.biologia@hotmail.com>
2022-09-05 21:05:02 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function generateColors(){
 
+
+//Função para gerar cores
+
+function generateColors(){
     const chars = '0123456789ABCDEF'
     let color = '#'
     for(let i = 0; i < 6; i += 1) {
@@ -8,12 +11,48 @@ function generateColors(){
     return color;
 }
 
-document.getElementById('button-random-color').addEventListener('click', () =>{
+// botão para gerar cores aleatoria
+document.getElementById('button-random-color').addEventListener('click', function btnColor (){
     for (let i = 2; i < 5; i += 1){
+        let corsave = generateColors()
         let divColor = document.getElementById(`color-${i}`)
-        divColor.style.backgroundColor = generateColors()
+        divColor.style.backgroundColor = corsave
     }
 })
 
-localStorage.setItem('colorPalette', 'color');
+
+function savecolor () {
+const storage = JSON.stringify(corsave);
+        localStorage.setItem('colorpallete', corsave)
+
+}
+function addSelect(event) {
+    const select = document.querySelector('.selected');
+    event.target.classList.add('selected');
+    select.classList.remove('selected');
+    input.value = '';
+  }
+
+function setPixelColour (pixel) {
+    pixel.style.backgroundColor = penColour;
+}
+
+document.getElementById("clear-board").addEventListener('click', function btnclear(){
+    const pixels = document.querySelectorAll('.pixel');
+    for (const pixel of pixels) {
+      pixel.style.backgroundColor = 'white';
+    }
+    }
+)
+function addSelect(event) {
+    const select = document.querySelector('.selected');
+    event.target.classList.add('selected');
+    select.classList.remove('selected');
+    input.value = '';
+  }
+  
+
+
+
+let penColour = 'blue';
 
